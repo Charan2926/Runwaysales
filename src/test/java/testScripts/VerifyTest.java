b994651daae1154b22bdebfd0357534de531e44a
Charan@2926 <charan1164@gmail.com>
2023-08-17 15:41:22 +0530

    hi

---
+++ src/test/java/testScripts/VerifyTest.java
@@ -0,0 +1,11 @@
+package testScripts;
+
+import org.testng.annotations.Test;
+
+public class VerifyTest {
+	@Test
+	public void verifycamp () {
+		System.out.println("verified data is sucessful");
+	}
+
+}
