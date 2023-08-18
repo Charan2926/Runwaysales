677a37a669e4dea9a7ac27683c544507bf1f3758
Charan@2926 <charan1164@gmail.com>
2023-08-18 14:48:56 +0530

    Create new camp

---
+++ src/test/java/testScripts/CreateCamp.java
@@ -0,0 +1,11 @@
+package testScripts;
+
+import org.testng.annotations.Test;
+
+public class CreateCamp {
+	@Test
+	public void createNewCamp() {
+		System.out.println("---------new camppaigns is created");
+	}
+
+}
